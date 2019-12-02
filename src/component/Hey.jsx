203cd979e9b6d916555ef2eb690390cefe0e6af1
Raphael Moita <raphael.moita@gmail.com>
2019-12-02 23:17:17 +0100

    new samples

--- src/component/Hey.jsx
+++ src/component/Hey.jsx
@@ -1,45 +1,80 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Hey extends Component {
-    state = {
-        message: "SUCCESS"
-    }
+  state = {
+    message: "WARNING",
+    options: ["Option-1", "Option-2", "Option-3"]
+  };
 
-    styles = {
-        fontSize: 30,
-        fontWeight: "bold"
-    }
+  styles = {
+    fontSize: 30,
+    fontWeight: "bold"
+  };
 
-    render() {
+  // it is being called by instance, not by method
+  handlerIncrement = () => {
+    console.log("Increment clicked");
+    this.setState({ message: (this.state.message = "DANGER") });
+  };
 
-        return (
-            <React.Fragment>
-                <span className={ this.createMessage }>{ this.wrapperIt() }</span>
-                <h2> =8-) </h2>
-            </React.Fragment>
-        );
-    }
-    
-    createMessage() {
-        const { message } = this.state; // object destructuring!
-        let classes = "badge ";
-        
-        if (message === "SUCCESS") {
-            classes += "badge-success";
-        }
-        else if (message === "WARNING") {
-            classes += "badge-warning";
-        }
-        else { // ERROR
-            classes += "badge-danger";
-        }
-        return classes;
-    }
+  // code inside is not javascript, it is jxs
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.createMessage()}>{this.wrapperIt()}</span>
+        <h2> =8-) </h2>
+        <ul>
+          {this.state.options.length === 0 && <p>No Options available!</p>}
+          {this.state.options.map(option => (
+            <li key={option}>{option}</li>
+          ))}
+          <button
+            onClick={this.handlerIncrement}
+            className="btn btn-secondary btn-sm"
+          >
+            Danger
+          </button>
+          <button
+            onClick={() =>
+              this.setState({ message: (this.state.message = "WARNING") })
+            }
+            className="btn btn-secondary btn-sm"
+          >
+            Warning
+          </button>
+          <button
+            onClick={() =>
+              this.setState({ message: (this.state.message = "SUCCESS") })
+            }
+            className="btn btn-secondary btn-sm"
+          >
+            Success
+          </button>
+        </ul>
+      </React.Fragment>
+    );
+  }
 
-    wrapperIt() {
-        const { message } = this.state; // object destructuring!
-        return message
+  // inside method is possible to use vanilla javascript
+  createMessage() {
+    const { message } = this.state; // object destructuring!
+    let classes = "badge ";
+
+    if (message === "SUCCESS") {
+      classes += "badge-success";
+    } else if (message === "WARNING") {
+      classes += "badge-warning";
+    } else {
+      // ERROR
+      classes += "badge-danger";
     }
+    return classes;
+  }
+
+  wrapperIt() {
+    const { message } = this.state; // object destructuring!
+    return message;
+  }
 }
 
 export default Hey;
